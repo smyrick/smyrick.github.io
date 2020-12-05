bd0a5fc6e154172617121156ef4c35f852258d0b
Shane Myrick <accounts@shanemyrick.com>
2020-12-05 15:58:32 -0800

    Init commit of gatsby template
    Ignore idea files
    
    Delete idea files
    
    Ignore IDE files
    
    Npm audit
    
    Init commit of new template
    
    Update profile image
    
    Use template gatsby-starter-julia

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-## Personal Landing Page
+# smyrick.github.io
 
-This is my personal landing page on the internet. It is a place to provide links to my other account and contact information. It is also viewable from my personal domain [shanemyrick.com](http://shanemyrick.com).
+This is my personal landing page on the internet.
+It is a place to provide links to my other accounts and contact information.
+It is also viewable from my personal domain [shanemyrick.com](http://shanemyrick.com).
 
-Inspired by the [stylish-portfolio](https://startbootstrap.com/template-overviews/stylish-portfolio/) bootstrap template
+This website is built with [Gatsby](https://www.gatsbyjs.com/) and the [gatsby-markdown-typescript-personal-website](https://www.gatsbyjs.com/starters/SaimirKapaj/gatsby-markdown-typescript-personal-website/).
+See [CONTRIBUTING.md](./CONTRIBUTING.md) for more details on how to build and run the site locally.
---
+++ gatsby-config.js
@@ -0,0 +1,54 @@
+module.exports = {
+  siteMetadata: {
+    title: `Shane Myrick`,
+    subtitle: `Software Developer and Fitness Coach`,
+    description: `A minimal blog starter built with Gatsbyjs. The needed Gatsby files are included.`,
+    author: `@smyrick`,
+  },
+  plugins: [
+    `gatsby-plugin-fontawesome-css`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/content`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [`gatsby-remark-reading-time`, {
+          resolve: `gatsby-remark-prismjs`,
+          options: {
+            aliases:{sh: "bash", js:"javascript"},
+            showLineNumbers: true,
+          }
+        }],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline'
+  ],
+}
